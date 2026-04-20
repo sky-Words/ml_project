728af8f5cd9cae417c86366ee10a3d26d818e094
elmehdi <elmehdi.sadki@outlook.fr>
2026-04-20 18:10:06 +0100

    add exception and logger

--- setup.py
+++ setup.py
@@ -1,29 +1,32 @@
-from setuptools import find_packages, setup
 from typing import List
 
+from setuptools import find_packages, setup
 
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+HYPHEN_E_DOT = "-e ."
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    
+
+def get_requirements(file_path: str) -> List[str]:
+    """Return requirements from a requirements file."""
+    requirements = []
+
+    with open(file_path, encoding="utf-8") as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+        requirements = [req for req in requirements if req and not req.startswith("#")]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
     return requirements
 
 
-setup(
-    name="first_ml_project",
-    version="0.1.0",
-    author="Elmehidi",
-    author_email="mehdi.sadki.dev@gmail.com",
-    packages=find_packages(),
-    install_requires=get_requirements("requirements.txt"),
-)
+if __name__ == "__main__":
+    setup(
+        name="first_ml_project",
+        version="0.1.0",
+        author="Elmehidi",
+        author_email="mehdi.sadki.dev@gmail.com",
+        packages=find_packages(),
+        install_requires=get_requirements("requirements.txt"),
+    )
--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from datetime import datetime
-
+import sys
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 LOG_PATH = os.path.join(os.getcwd(), "logs", LOG_FILE)
@@ -13,3 +13,13 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
+## just tst logging
+# if __name__ == "__main__":
+#     logging.info("Logging has started.")
+
+#     try:
+#         result = 10 / 0
+#     except Exception as e:
+#         logging.error(f"An error occurred: {e}")
+#         raise ChildProcessError(e,sys)
